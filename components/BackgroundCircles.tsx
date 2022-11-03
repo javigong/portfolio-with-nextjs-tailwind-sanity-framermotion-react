7d2f7a88ad81877dd38ecae7eb5c14eb01206230
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-03 16:51:37 -0700

    Update BackgroundCircles.tsx

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -18,11 +18,11 @@ const BackgroundCircles = (props: Props) => {
         duration: 2.5,
       }}
     >
-      <div className="absolute border border-gray-300 rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
-      <div className="absolute border border-black-300 rounded-full h-[300px] w-[300px] mt-52" />
-      <div className="absolute border border-gray-300 rounded-full h-[500px] w-[500px] mt-52" />
-      <div className="absolute border border-black-300 rounded-full h-[650px] w-[650px]  mt-52 animate-pulse" />
-      <div className="absolute border border-gray-300 rounded-full h-[800px] w-[800px]  mt-52" />
+      <div className="absolute border border-gray-100 rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
+      <div className="absolute border border-gray-100 rounded-full h-[300px] w-[300px] mt-52" />
+      <div className="absolute border border-gray-100 rounded-full h-[500px] w-[500px] mt-52" />
+      <div className="absolute border border-gray-200 rounded-full h-[650px] w-[650px]  mt-52" />
+      <div className="absolute border border-gray-100 rounded-full h-[800px] w-[800px]  mt-52" />
     </motion.div>
   );
 };
