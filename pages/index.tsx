f5a42e4d95406157fb8c6a882ed1aab78c4a49d8
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-03 16:52:04 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import Head from "next/head";
 import About from "../components/About";
+import ContactMe from "../components/ContactMe";
+import Experience from "../components/Experience";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
 
 type Props = {};
 
 const Home = (props: Props) => {
   return (
-    <div className="bg-[#ede8e824] text-black h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div className="bg-[#ede8e824] text-black h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-[#ede8e824] scrollbar-thumb-green-500/30">
       <Head>
         <title>Javier's Portfolio</title>
       </Head>
@@ -22,16 +26,26 @@ const Home = (props: Props) => {
 
       {/* About */}
       <section id="about" className="snap-center">
-        <About/>
+        <About />
       </section>
 
       {/* Experience */}
+      <section id="experience" className="snap-center">
+        <Experience />
+      </section>
 
       {/* Skills */}
-
+      <section id="skills" className="snap-start">
+        <Skills />
+      </section>
       {/* Projects */}
-
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
       {/* Contact Me */}
+      <section id="contact" className="snap-start">
+        <ContactMe />
+      </section>
     </div>
   );
 };
