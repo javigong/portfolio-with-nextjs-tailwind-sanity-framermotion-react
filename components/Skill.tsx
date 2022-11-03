71f4124e65c17e6765ae7de2ba14d29498914c0e
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-03 16:51:51 -0700

    Create Skill.tsx

---
+++ components/Skill.tsx
@@ -0,0 +1,36 @@
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+type Props = {
+  directionLeft?: boolean;
+};
+
+const Skill = ({ directionLeft }: Props) => {
+  return (
+    <div className="group relative flex cursor-pointer">
+      <motion.div
+        initial={{
+          x: directionLeft ? -200 : 200,
+          opacity: 0,
+        }}
+        transition={{ duration: 1 }}
+        whileInView={{ opacity: 1, x: 0 }}
+      >
+        <Image
+          className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+          src="https://avatars.githubusercontent.com/u/42308135?v=4"
+          alt="Javier Gongora'"
+          width={80}
+          height={80}
+        />
+      </motion.div>
+      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
+        <div className="flex items-center justify-center h-full">
+          <p className="text-3xl font-bold text-black opacity-100 ">100%</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skill;
