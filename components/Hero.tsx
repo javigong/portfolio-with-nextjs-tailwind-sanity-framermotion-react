56f5aad9d7fc0c7a9bdb2368edbd1009323d7b7f
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-03 16:51:45 -0700

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -20,7 +20,7 @@ const Hero = (props: Props) => {
   });
   return (
     <>
-      <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <div className="h-screen flex flex-col space-y-10 items-center justify-center text-center overflow-hidden">
         <BackgroundCircles />
         <Image
           className="relative rounded-full h-32 w-32 mx-auto object-cover"
