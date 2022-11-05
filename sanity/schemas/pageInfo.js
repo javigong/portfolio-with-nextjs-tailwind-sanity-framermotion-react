306667f6f9db680b6e55a312ca62ff0d4f3f6f01
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-04 19:30:41 -0700

    Add schemas to sanity

---
+++ sanity/schemas/pageInfo.js
@@ -0,0 +1,49 @@
+export default {
+  name: "pageInfo",
+  title: "PageInfo",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "role",
+      title: "Role",
+      type: "string",
+    },
+    {
+      name: "heroImage",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "backgroundInformation",
+      title: "BackgroundInformation",
+      type: "text",
+    },
+    {
+      name: "profilePic",
+      title: "profilePic",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "address",
+      title: "Address",
+      type: "string",
+    },
+    {
+      name: "socials",
+      title: "Socials",
+      type: "array",
+      of: [{ type: "reference", to: { type: "social" } }],
+    },
+  ],
+};
