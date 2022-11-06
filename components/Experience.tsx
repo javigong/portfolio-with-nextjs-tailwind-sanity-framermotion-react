a0ad934fc47abcb4699214673b739e7a3e6c496a
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-05 17:54:49 -0700

    Update Experience.tsx

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,22 +1,25 @@
+import { Experience } from "../typings";
 import ExperienceCard from "./ExperienceCard";
 
-type Props = {};
+type Props = {
+  experiences: Experience[];
+};
 
-const Experience = (props: Props) => {
+const WorkExperience = ({ experiences }: Props) => {
   return (
     <div className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Experience
       </h3>
-    <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x scrollbar scrollbar-track-[#ede8e824] scrollbar-thumb-green-500/30">
-      <ExperienceCard/>
-      <ExperienceCard/>
-      <ExperienceCard/>
-      <ExperienceCard/>
-    </div>
-
+      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x scrollbar scrollbar-track-[#ede8e824] scrollbar-thumb-green-500/30">
+        {experiences.map((experience) => {
+          return (
+            <ExperienceCard key={experience._id} experience={experience} />
+          );
+        })}
+      </div>
     </div>
   );
 };
 
-export default Experience;
+export default WorkExperience;
