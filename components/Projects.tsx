8414bc9403379cb679aa3a83076406154c21a63f
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-05 17:54:59 -0700

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,10 +1,13 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
+import { urlFor } from "../sanity";
+import { Project } from "../typings";
 
-type Props = {};
+type Props = {
+  projects: Project[];
+};
 
-const Projects = (props: Props) => {
-  const projects = [1, 2, 3, 4, 5];
+const Projects = ({ projects }: Props) => {
   return (
     <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
@@ -13,7 +16,10 @@ const Projects = (props: Props) => {
 
       <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar-thin scrollbar-track-[#ede8e824] scrollbar-thumb-green-500/50">
         {projects.map((project, i) => (
-          <div key={project} className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
+          <div
+            key={project?._id}
+            className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
+          >
             <motion.div
               initial={{
                 y: -300,
@@ -25,29 +31,34 @@ const Projects = (props: Props) => {
             >
               <Image
                 className="h-666 w-666"
-                src="https://avatars.githubusercontent.com/u/42308135?v=4"
-                alt="Javier Gongora'"
+                src={urlFor(project?.image).url()}
+                alt={project?.title}
                 width={222}
                 height={222}
               />
             </motion.div>
 
-            <div className="space-y-10 px-0 md md:px-10 max-w-6xl">
+            <div className="space-y-10 px-0 md:px-10 max-w-6xl">
               <h4 className="text-4xl font-semibold text-center">
                 <span className="font-bold">
                   Case Study {i + 1} of {projects.length}:
                 </span>{" "}
-                UPS clone
+                {project?.title}
               </h4>
-              <p className="">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
+              <div className="flex flex-wrap flex-0 justify-center align-items">
+                {project?.technologies?.map((technology) => (
+                  <div className="ml-2 my-1">
+                    <Image
+                      className="h-10 w-10 rounded-full"
+                      src={urlFor(technology?.image).url()}
+                      alt={technology?.title}
+                      width={40}
+                      height={40}
+                    />
+                  </div>
+                ))}
+              </div>
+              <p>{project?.summary}</p>
             </div>
           </div>
         ))}
