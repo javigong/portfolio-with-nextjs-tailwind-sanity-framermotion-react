c0bbcdc01865daa64afc6581e98332f5e652f190
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-05 17:54:51 -0700

    Update ExperienceCard.tsx

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,9 +1,13 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
+import { urlFor } from "../sanity";
+import { Experience } from "../typings";
 
-type Props = {};
+type Props = {
+  experience: Experience;
+};
 
-const ExperienceCard = (props: Props) => {
+const ExperienceCard = ({ experience }: Props) => {
   return (
     <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#acc49d1c] p-10 opacity-40 hover:opacity-100 cursor-pointer transition-opacity duration-200 overflow-hidden">
       <motion.div
@@ -17,62 +21,39 @@ const ExperienceCard = (props: Props) => {
         viewport={{ once: true }}
       >
         <Image
-          className="w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
-          src="https://avatars.githubusercontent.com/u/42308135?v=4"
-          alt="Javier Gongora'"
-          width={128 * 3}
-          height={128 * 3}
+          className="w-32 h-32 rounded-full xl:w-[128px] xl:h-[128px] object-contain object-center"
+          src={urlFor(experience?.companyImage).url()}
+          alt={experience?.company}
+          width={128}
+          height={128}
         />
       </motion.div>
       <div className="px-0 md:px-10">
-        <h4 className="text-4xl font-light ">CEO of PAPAFAM</h4>
-        <p className="font-bold text-2xl mt-1">PAPAFAM</p>
-        <div className="flex space-x-2 my-2">
-          {/* tech used */}
-          <div>
-            <Image
-              className="h-10 w-10 rounded-full"
-              src="https://avatars.githubusercontent.com/u/42308135?v=4"
-              alt="Javier Gongora'"
-              width={40}
-              height={40}
-            />
-          </div>
-          {/* tech used */}
-          <div>
-            <Image
-              className="h-10 w-10 rounded-full"
-              src="https://avatars.githubusercontent.com/u/42308135?v=4"
-              alt="Javier Gongora'"
-              width={40}
-              height={40}
-            />
-          </div>
-          {/* tech used */}
-          <div>
-            <Image
-              className="h-10 w-10 rounded-full"
-              src="https://avatars.githubusercontent.com/u/42308135?v=4"
-              alt="Javier Gongora'"
-              width={40}
-              height={40}
-            />
-          </div>
-          {/* tech used */}
-          <div>
-            <Image
-              className="h-10 w-10 rounded-full"
-              src="https://avatars.githubusercontent.com/u/42308135?v=4"
-              alt="Javier Gongora'"
-              width={40}
-              height={40}
-            />
-          </div>
+        <h4 className="text-4xl font-light ">{experience?.jobTitle}</h4>
+        <p className="font-bold text-2xl mt-1">{experience?.company}</p>
+        <div className="flex flex-wrap flex-0 justify-left align-items">
+          {experience?.technologies?.map((technology) => (
+            <div className="ml-2 my-1">
+              <Image
+                className="h-10 w-10 rounded-full"
+                src={urlFor(technology?.image).url()}
+                alt={technology?.title}
+                width={40}
+                height={40}
+              />
+            </div>
+          ))}
         </div>
-        <p className="uppercase py-5 text-black">Stared work... - Ended...</p>
-        <ul className="list-disc space-y-4 ml-5 text-lg">
-          <li>Summary points Summary points Summary points Summary points</li>
-          <li>Summary points Summary points Summary points Summary points</li>
+        <p className="uppercase py-5 text-black">
+          {new Date(experience?.dateStarted).toLocaleDateString("en-CA")} -{" "}
+          {experience.isCurrentlyWorkingHere
+            ? "Present"
+            : new Date(experience?.dateEnded).toLocaleDateString("en-CA")}
+        </p>
+        <ul className="list-disc space-y-4 ml-5 text-lg max-h-96">
+          {experience?.points?.map((point) => (
+            <li key={point}>{point}</li>
+          ))}
         </ul>
       </div>
     </article>
