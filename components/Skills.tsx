c587b1375608386f82c41e07ff37dc31ce277de6
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-05 17:55:03 -0700

    Update Skills.tsx

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,9 +1,12 @@
 import { motion } from "framer-motion";
-import Skill from "./Skill";
+import { Skill } from "../typings";
+import SkillComponent from "./Skill";
 
-type Props = {};
+type Props = {
+  skills: Skill[];
+};
 
-const Skills = (props: Props) => {
+const Skills = ({ skills }: Props) => {
   return (
     <motion.div
       className="h-screen flex relative overflow-hidden flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
@@ -18,17 +21,12 @@ const Skills = (props: Props) => {
         Hover over a skill for current proficiency
       </h3>
       <div className="grid grid-cols-4 gap-5">
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
+        {skills?.slice(0, skills.length / 2).map((skill) => (
+          <SkillComponent key={skill._id} skill={skill} />
+        ))}
+        {skills?.slice(skills.length / 2, skills.length).map((skill) => (
+          <SkillComponent key={skill._id} skill={skill} directionLeft/>
+        ))}
       </div>
     </motion.div>
   );
