b28359161ab4755eb2517bf528ca32e20a4a6a9a
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-05 17:55:01 -0700

    Update Skill.tsx

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,11 +1,14 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
+import { urlFor } from "../sanity";
+import { Skill } from "../typings";
 
 type Props = {
+  skill: Skill;
   directionLeft?: boolean;
 };
 
-const Skill = ({ directionLeft }: Props) => {
+const Skill = ({ skill, directionLeft }: Props) => {
   return (
     <div className="group relative flex cursor-pointer">
       <motion.div
@@ -17,16 +20,16 @@ const Skill = ({ directionLeft }: Props) => {
         whileInView={{ opacity: 1, x: 0 }}
       >
         <Image
-          className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
-          src="https://avatars.githubusercontent.com/u/42308135?v=4"
-          alt="Javier Gongora'"
+          className="rounded-full border border-gray-500 object-contain w-20 h-20 md:w-20 md:h-20 xl:w-25 xl:h-25 filter group-hover:grayscale transition duration-300 ease-in-out"
+          src={urlFor(skill?.image).url()}
+          alt={skill?.title}
           width={80}
           height={80}
         />
       </motion.div>
-      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
+      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-20 h-20 md:w-20 md:h-20 xl:w-25 xl:h-25 rounded-full z-0">
         <div className="flex items-center justify-center h-full">
-          <p className="text-3xl font-bold text-black opacity-100 ">100%</p>
+          <p className="text-3xl font-bold text-black opacity-100 ">{skill?.progress}%</p>
         </div>
       </div>
     </div>
