01270489a8fcb60d68e15d6ebdac10dbbeb95889
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-05 17:55:19 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,54 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import { useEffect } from "react";
 import About from "../components/About";
 import ContactMe from "../components/ContactMe";
-import Experience from "../components/Experience";
+import WorkExperience from "../components/Experience";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
+import { Experience, PageInfo, Project, Skill, Social } from "../typings";
+import { fetchExperiences } from "../utils/fetchExperiences";
+import { fetchPageInfo } from "../utils/fetchPageInfo";
+import { fetchProjects } from "../utils/fetchProjects";
+import { fetchSkills } from "../utils/fetchSkills";
+import { fetchSocials } from "../utils/fetchSocials";
 
-type Props = {};
+type Props = {
+  pageInfo: PageInfo;
+  experiences: Experience[];
+  skills: Skill[];
+  projects: Project[];
+  socials: Social[];
+};
 
-const Home = (props: Props) => {
-
-
+const Home = ({ pageInfo, experiences, skills, projects, socials }: Props) => {
   return (
     <div className="bg-[#ede8e824] text-black h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-[#ede8e824] scrollbar-thumb-green-500/30 scroll-smooth">
       <Head>
         <title>Javier's Portfolio</title>
       </Head>
-      <Header />
+      <Header socials={socials} />
 
       <section id="hero" className="snap-start">
-        <Hero />
+        <Hero pageInfo={pageInfo} />
       </section>
 
       <section id="about" className="snap-center">
-        <About />
+        <About pageInfo={pageInfo} />
       </section>
 
       <section id="experience" className="snap-center">
-        <Experience />
+        <WorkExperience experiences={experiences} />
       </section>
 
       <section id="skills" className="snap-start">
-        <Skills />
+        <Skills skills={skills} />
       </section>
 
       <section id="projects" className="snap-start">
-        <Projects />
+        <Projects projects={projects} />
       </section>
 
       <section id="contact" className="snap-start">
@@ -60,3 +70,25 @@ const Home = (props: Props) => {
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const pageInfo: PageInfo = await fetchPageInfo();
+  const experiences: Experience[] = await fetchExperiences();
+  const skills: Skill[] = await fetchSkills();
+  const projects: Project[] = await fetchProjects();
+  const socials: Social[] = await fetchSocials();
+
+  return {
+    props: {
+      pageInfo,
+      experiences,
+      skills,
+      projects,
+      socials,
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most every 60 seconds
+    revalidate: 60, // In seconds
+  };
+};
